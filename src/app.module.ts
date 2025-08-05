83f8c2cbd1fe3cbc8262995d5d0e9313b8358e4d
Konstantin Alekseev <alexcseev1990@gmail.com>
2025-08-05 17:40:48 +0300

    add docker file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TaskModule } from './task/task.module';
-
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TaskModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'root',
+      password: '123456',
+      database: "nestjs-api",
+      autoLoadEntities: true,
+      synchronize: true,
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
